1ee454c7021a3e57b74debae5342723647c04d63
LORD of HENTAI <v2508991999@gmail.com>
2022-06-20 14:15:23 +0300

    com3

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,7 +20,7 @@ interface partnersPic {
 export class HomeComponent implements OnInit {
 
   linksArr: mainLinks[] = [
-    { id: 1, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
+    { id: 1, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit. dfgdfgdfgsfdgsfgsfdgsfdgsfdgsdf' },
     { id: 2, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
     { id: 3, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
   ]
