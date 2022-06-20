15adddc62c72f314f9d2f20be27190514c1fbdbf
LORD of HENTAI <v2508991999@gmail.com>
2022-06-20 10:40:48 +0300

    com2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,11 @@ interface mainLinks {
   text: string,
 }
 
+interface partnersPic {
+  id: number,
+  picPath: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,7 +25,12 @@ export class HomeComponent implements OnInit {
     { id: 3, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
   ]
 
-
+  partnersArr: partnersPic[] = [
+    { id: 1, picPath: '/../assets/1.jpg' },
+    { id: 2, picPath: '/../assets/2.jpg' },
+    { id: 3, picPath: '/../assets/3.jpg' },
+    { id: 4, picPath: '/../assets/4.jpg' },
+    { id: 5, picPath: '/../assets/1.jpg' }]
 
   constructor() { }
 
