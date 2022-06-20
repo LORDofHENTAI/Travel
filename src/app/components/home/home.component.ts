a24aeb684202a7ef81b601fc0ef5913a2033e20b
LORD of HENTAI <v2508991999@gmail.com>
2022-06-20 10:34:35 +0300

    com2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface mainLinks {
+  id: number,
+  pathPic: string,
+  title: string,
+  text: string,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  linksArr: mainLinks[] = [
+    { id: 1, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
+    { id: 2, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
+    { id: 3, pathPic: '/../assets/welcomLink.jpg', title: 'Lorem', text: 'Lorem ipsum dolor sit.' },
+  ]
+
+
 
   constructor() { }
 
